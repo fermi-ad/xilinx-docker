f9b4f61a43ba427949915aafd79edf7cf1395922
Jason Moss <jason.moss@avnet.com>
2019-02-26 11:53:54 -0500

    Adding base image recipes for 18.04.2.  Vivado v2018.3 advertises support for 16.04.4 and 18.04.2, however the VCU TRD has issues in Ubuntu 16.04.4 that I've been unable to resolve so migrating to an Ubuntu 18.04.2 base image for Vivado 2018.3 container.

---
+++ recipes/base-images/ubuntu-18.04.2/build_image.ps1
@@ -0,0 +1,62 @@
+# CONVERTED ORIGINAL BASH SCRIPT TO WINDOWS POWERSHELL SYNTAX
+#!/bin/bash
+########################################################################################
+# Docker Image Build Script:
+#	- Create a base ubuntu image from a specific release tarball
+#   - Base image tarballs for older releases can be found in the cdimage archive:
+#		Link: http://cdimage.ubuntu.com/ubuntu-base/releases/16.04/release/
+#
+# Maintainer:
+#	- Jason Moss (jason.moss@avnet.com)
+#	- Xilinx Applications Engineer, Embedded Software
+#
+# Created: 
+#	- 2/18/2019
+#
+. ".\include\configuration.ps1"
+
+# Check for downloaded tarball
+if (Test-Path "depends/$UBUNTU_RELEASE_ARCHIVE") {
+		echo "Base Release Image [Good] $UBUNTU_RELEASE_ARCHIVE"
+} else {
+	echo "Base Release Image [Missing] $UBUNTU_RELEASE_ARCHIVE"	
+	mkdir -Force depends
+	echo "Attempting to download $UBUNTU_BASE_URL/$UBUNTU_VERSION/release/$UBUNTU_RELEASE_ARCHIVE"
+
+	if ($BUILD_DEBUG -ne 0) {
+	  # Turn on debug tracing (this script)
+	  Set-PSDebug -Trace 1
+	}
+
+	wget "$UBUNTU_BASE_URL/$UBUNTU_VERSION/release/$UBUNTU_RELEASE_ARCHIVE" -Outfile "depends/$UBUNTU_RELEASE_ARCHIVE"
+
+	if ($BUILD_DEBUG -ne 0) {
+	  # Turn on debug tracing (this script)
+	  Set-PSDebug -Trace 0
+	}
+
+	if (Test-Path "depends/$UBUNTU_RELEASE_ARCHIVE") {
+		echo "Base Relese Image Download [Good] "$UBUNTU_RELEASE_ARCHIVE
+	} else {
+		echo "Base Release Image Download [failed] "$UBUNTU_RELEASE_ARCHIVE
+		exit $EX_OSFILE
+	}
+}
+
+if ($BUILD_DEBUG -ne 0) {
+  # Turn on debug tracing (this script)
+  Set-PSDebug -Trace 1
+}
+
+# Import into a new image
+docker import "depends/$UBUNTU_RELEASE_ARCHIVE" "ubuntu:$UBUNTU_VERSION"
+# Show docker images
+docker image ls -a
+# Show System disk use
+docker system df
+
+if ($BUILD_DEBUG -ne 0) {
+  # Turn on debug tracing (this script)
+  Set-PSDebug -Trace 0
+}
+
---
+++ recipes/base-images/ubuntu-18.04.2/build_image.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+########################################################################################
+# Docker Image Build Script:
+#	- Create a base ubuntu image from a specific release tarball
+#   - Base image tarballs for older releases can be found in the cdimage archive:
+#		Link: http://cdimage.ubuntu.com/ubuntu-base/releases/18.04/release/
+#
+# Maintainer:
+#	- Jason Moss (jason.moss@avnet.com)
+#	- Xilinx Applications Engineer, Embedded Software
+#
+# Created: 
+#	- 2/18/2019
+#
+source include/configuration.sh
+
+# Check for downloaded tarball
+if [ -f depends/$UBUNTU_RELEASE_ARCHIVE ] || [ -L depends/$UBUNTU_RELEASE_ARCHIVE ]; then
+	echo "Base Release Image [Good] "$UBUNTU_RELEASE_ARCHIVE
+else
+	echo "Base Release Image [Missing] "$UBUNTU_RELEASE_ARCHIVE
+	mkdir -p depends
+	echo "Attempting to download "$UBUNTU_BASE_URL"/"$UBUNTU_VERSION"/release/"$UBUNTU_RELEASE_ARCHIVE
+	
+	if [ $BUILD_DEBUG -ne 0 ]; then set -x; fi
+	
+	wget $UBUNTU_BASE_URL/$UBUNTU_VERSION/release/$UBUNTU_RELEASE_ARCHIVE -O depends/$UBUNTU_RELEASE_ARCHIVE
+	
+	if [ $BUILD_DEBUG -ne 0 ]; then set +x; fi
+
+	if [ -f depends/$UBUNTU_RELEASE_ARCHIVE ]; then
+		echo "Base Relese Image Download [Good] "$UBUNTU_RELEASE_ARCHIVE
+	else
+		echo "Base Release Image Download [failed] "$UBUNTU_RELEASE_ARCHIVE
+		exit $EX_OSFILE
+	fi
+fi
+
+if [ $BUILD_DEBUG -ne 0 ]; then set -x; fi
+
+# Import into a new image
+docker import depends/$UBUNTU_RELEASE_ARCHIVE ubuntu:$UBUNTU_VERSION
+# Show docker images
+docker image ls -a
+# Show docker disk use
+docker system df
+
+if [ $BUILD_DEBUG -ne 0 ]; then set +x; fi
+
